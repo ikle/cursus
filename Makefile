46719c9483c5cebe7add6d6c411129991216eddd
Alexei A. Smekalkine <ikle@ikle.ru>
2023-11-04 04:07:07 +0300

    Add generic output buffer implementation

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ URL = https://github.com/ikle/yonk-io
 
 LIBNAME	= yonk-io
 LIBVER	= 0
-LIBREV	= 0.4
+LIBREV	= 0.5
 
 include make-core.mk
---
+++ include/yonk/io/obuf.h
@@ -0,0 +1,55 @@
+/*
+ * Yonk I/O Output Buffer
+ *
+ * Copyright (c) 2023 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef YONK_IO_OBUF_H
+#define YONK_IO_OBUF_H  1
+
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct obuf;
+
+typedef int obuf_cb (struct obuf *o, int count);
+
+struct obuf {
+	char *head, *cursor, *tail;
+	obuf_cb *cb;
+};
+
+static inline int obuf_init (struct obuf *o, size_t size, obuf_cb cb)
+{
+	if ((o->head = malloc (size)) == NULL)
+		return 0;
+
+	o->tail   = o->head + size;
+	o->cursor = o->tail;
+	o->cb     = cb;
+	return 1;
+}
+
+static inline void obuf_fini (struct ibuf *o)
+{
+	free (o->head);
+}
+
+#ifndef MIN
+#define MIN(x, y)  (((x) < (y)) ? (x) : (y))
+#endif
+
+static inline int obuf_pull (struct obuf *o, void *data, size_t count)
+{
+	count = MIN (MIN (count, INT_MAX), o->tail - o->cursor);
+
+	memcpy (data, o->cursor, count);
+
+	o->cursor += count;
+	return o->cb (o, count);
+}
+
+#endif  /* YONK_IO_OBUF_H */
