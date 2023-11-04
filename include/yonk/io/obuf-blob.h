d774f2cf6406bb00f7052d99d37dc7f2233fb937
Alexei A. Smekalkine <ikle@ikle.ru>
2023-11-04 04:15:48 +0300

    obuf: add blob output buffer implementation

---
+++ include/yonk/io/obuf-blob.h
@@ -0,0 +1,33 @@
+/*
+ * Yonk I/O Blob Output Buffer
+ *
+ * Copyright (c) 2023 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef YONK_IO_OBUF_BLOB_H
+#define YONK_IO_OBUF_BLOB_H  1
+
+#include <yonk/io/obuf.h>
+
+static inline int obuf_blob_cb (struct obuf *o, int count)
+{
+	return count > 0 && o->tail > o->cursor;
+}
+
+static inline int obuf_blob_init (struct obuf *o, const void *data, size_t size)
+{
+	o->head   = (void *) data;
+	o->cursor = o->head;
+	o->tail   = o->head + size;
+	o->cb     = obuf_blob_cb;
+	return 1;
+}
+
+static inline void obuf_blob_fini (struct obuf *o)
+{
+	/* nothing */
+}
+
+#endif  /* YONK_IO_OBUF_BLOB_H */
