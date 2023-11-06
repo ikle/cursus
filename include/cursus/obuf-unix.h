3999ef6017988dfb38832c8f61fe604195b0d955
Alexei A. Smekalkine <ikle@ikle.ru>
2023-11-06 03:24:08 +0300

    obuf-unix: fix polling condition, prevent deadlock

--- include/cursus/obuf-unix.h
+++ include/cursus/obuf-unix.h
@@ -26,7 +26,7 @@ static inline int obuf_pull_file (struct obuf *o, int fd)
 
 static inline int obuf_do_poll (struct obuf *o, struct pollfd *p)
 {
-	if ((p->revents | POLLOUT) == 0 || obuf_pull_file (o, p->fd))
+	if ((p->revents & (POLLOUT|POLLERR)) == 0 || obuf_pull_file (o, p->fd))
 		return p->events;
 
 	return p->events &= ~POLLOUT;
